5568ce010779be160385221887c0e76cda8b6dcb
akilalipa-cyber <akil.alipa@gmail.com>
2026-02-09 11:30:26 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const users = [
+  { username: "gate", password: "123", role: "gerbang" },
+  { username: "operator", password: "123", role: "operator" },
+  { username: "wali", password: "123", role: "wali" },
+  { username: "guru", password: "123", role: "guru" },
+  { username: "aqyl", password: "123", role: "siswa" }
+];
+
+let absensi = [];
